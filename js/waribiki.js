2ac3cf05956216ce13337bcdac7c6a6e66f10199
maeponyo <maeponyo@gmail.com>
2014-05-08 11:53:05 +0900

    Create waribiki.js

---
+++ js/waribiki.js
@@ -0,0 +1,5 @@
+var inputOperantA = document.querySelector("#operant_a");
+var inputOperantB = document.querySelector("#operant_b");
+var inputOperator = document.querySelector("#operator");
+var output = document.querySelector("#output");
+var error = document.querySelector("#error");
