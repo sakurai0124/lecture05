31ee1254b6462a1347a99c77e6874c4c22b1658d
maeponyo <maeponyo@gmail.com>
2014-05-08 11:57:32 +0900

    Delete waribiki.js

--- js/waribiki.js
+++
@@ -1,5 +0,0 @@
-var inputOperantA = document.querySelector("#operant_a");
-var inputOperantB = document.querySelector("#operant_b");
-var inputOperator = document.querySelector("#operator");
-var output = document.querySelector("#output");
-var error = document.querySelector("#error");
