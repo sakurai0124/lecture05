aaa48efbf16aa362ab5617084efa46fdb6a60802
sakurai0124 <s13388ns@sfc.keio.ac.jp>
2014-05-21 14:28:06 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,11 @@ var inputOperator = document.querySelector("#operator");
 var output = document.querySelector("#output");
 var error = document.querySelector("#error");
 
+
+// 5% 8% で２つの商品の値段を消費税計算、レジのようにその小計を出す。（どちらかに数値を入れなかった場合エラー）
+
+
+
 var showError = function(){
 	error.setAttribute("class", "");
 };
@@ -36,9 +41,18 @@ var modulus = function(a, b){
 	return a % b;
 };
 
+var shohizei5 = function(a, b){
+	return a * 1.05 + b * 1.05 ;
+};
+
+var shohizei8 = function(a, b){
+	return a * 1.08 + b * 1.08 ;
+};
+
+
 var isOperator = function(operator){
 	return operator == "+" || operator == "-" || operator == "*" ||
-	operator == "/" || operator == "%";
+	operator == "/" || operator == "%" || operator == "5%" || operator == "8%";
 };
 
 var isNumber = function(a){
@@ -65,6 +79,14 @@ var isModulus = function(operator, a, b){
 	return operator == "%" && isNaturalNumber(a) && isNaturalNumber(b);
 };
 
+var isShohizei5 = function(operator, a, b){
+	return operator == "5%" && isNaturalNumber(a) && isNaturalNumber(b);
+};
+var isShohizei8 = function(operator, a, b){
+	return operator == "8%" && isNaturalNumber(a) && isNaturalNumber(b);
+};
+
+
 var isMultiplication = function(operator, a, b){
 	return operator == "*" && isNumber(a) && isNumber(b);
 };
@@ -77,9 +99,13 @@ var isAddition = function(operator, a, b){
 	return operator == "+" && isNumber(a) && isNumber(b);
 }
 
+
+
+
 var isReady = function(operator, a, b){
 	return isDivision(operator, a, b) || isModulus(operator, a, b) ||
-	isMultiplication(operator, a, b) || isSubtraction(operator, a, b) || isAddition(operator, a, b);
+	isMultiplication(operator, a, b) || isSubtraction(operator, a, b) || isAddition(operator, a, b) 
+|| isShohizei5(operator, a, b) || isShohizei8(operator, a, b);
 };
 
 var startCalc = function(){
@@ -103,6 +129,11 @@ var startCalc = function(){
 			result = divide(operandA, operandB);
 		}else if(operator == "%"){
 			result = modulus(operandA, operandB);
+				}else if(operator == "5%"){
+			result = shohizei5(operandA, operandB);
+					}else if(operator == "8%"){
+			result = shohizei8(operandA, operandB);
+			
 		}
 		showResult(result);
 	}else{
